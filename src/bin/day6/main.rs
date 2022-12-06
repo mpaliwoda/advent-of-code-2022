21ca5c0563cb1629563c9d2201fcd9339da9554e
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2022-12-06 12:58:15 +0100

    Day 6

---
+++ src/bin/day6/main.rs
@@ -0,0 +1,44 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("./src/bin/day6/input.txt").expect("caveman brain forgot");
+
+    let input = input.trim();
+    let chars: Vec<char> = input.chars().collect();
+
+    println!("Part 1: {}", find_marker(&chars, 4));
+    println!("Part 1: {}", find_marker(&chars, 14));
+}
+
+fn find_marker(buffer: &Vec<char>, marker_len: usize) -> usize {
+    let mut marker_ix: usize = marker_len;
+
+    let mut start: usize = 0;
+    let mut end: usize = 1;
+
+    let mut seq: Vec<&char> = Vec::new();
+
+    seq.push(&buffer.iter().next().unwrap());
+
+    while start < buffer.len() - marker_len && end < buffer.len() {
+        if seq.len() == marker_len {
+            marker_ix = start + marker_len;
+            break;
+        }
+
+        let current = &buffer[end];
+
+        if seq.contains(&current) {
+            seq.clear();
+            start = start + 1;
+            end = start + 1;
+            seq.push(&buffer[start]);
+            continue;
+        }
+
+        end += 1;
+        seq.push(&current);
+    }
+
+    return marker_ix;
+}
